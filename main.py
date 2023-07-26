194d7081657d52e7b7dfa6ba1b27e80c0f44c15b
FHS19085 <106130232+FHS19085@users.noreply.github.com>
2023-07-26 03:03:16 +0000

    Started to code my program

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Test")
+from tkinter import *
+
+
+class Converter:
+	
+
+# Main Routine
+if __name__ == "__main__":
+	root = Tk()
+	root.title("(Quiz Game Title)")
+	Converter()
+	root.mainloop()
